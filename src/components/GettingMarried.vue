e17d72625df29fe2d5e897f5369264f71ea9d5d6
D.G.BANG <lubang@lulab.net>
2017-09-03 16:24:00 +0900

    Disqus Comment 추가

--- src/components/GettingMarried.vue
+++ src/components/GettingMarried.vue
@@ -8,18 +8,21 @@
     </div>
     <gallery-view id="photo"></gallery-view>
     <navigation-view id="bus"></navigation-view>
+    <guestbook-view></guestbook-view>
   </div>
 </template>
 
 <script>
 import Gallery from '../components/Gallery';
 import Navigation from '../components/Navigation';
+import Guestbook from '../components/Guestbook';
 
 export default {
   name: 'getting-married',
   components: {
     'gallery-view': Gallery,
     'navigation-view': Navigation,
+    'guestbook-view': Guestbook,
   },
   data() {
     return {
